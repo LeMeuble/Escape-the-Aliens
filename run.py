4b20f2f5ab998cb478dcc5e7678daebea44a72b9
AlitaProject <64663369+AlitaProject@users.noreply.github.com>
2020-06-08 11:46:41 +0200

    Initial commit

---
+++ run.py
@@ -0,0 +1,16 @@
+# ------------------ ALIEN ESCAPE V1.0 ------------------ #
+#
+#
+#
+#
+#
+# ------------------------------------------------------- #
+
+
+import pygame
+
+from pygame.locals import *
+
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
