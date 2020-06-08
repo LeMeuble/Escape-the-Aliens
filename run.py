f199ea93a0c1e2ac3217bedb55ab959851fe46bd
AlitaProject <64663369+AlitaProject@users.noreply.github.com>
2020-06-08 13:22:03 +0200

    Patterns

--- run.py
+++ run.py
@@ -8,6 +8,7 @@
 
 
 import pygame
+import sys
 
 from pygame.locals import *
 
@@ -16,3 +17,21 @@ WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 WINDOW_FRAMERATE = 60
 WINDOW_FLAGS = None
+
+CANVAS_WIDTH = 600
+CANVAS_HEIGHT = WINDOW_HEIGHT
+
+RUN = True
+
+
+OBJ_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
+while RUN:
+
+	for e in pygame.event.get():
+
+		if e.type == pygame.QUIT:
+
+			RUN = False
+			sys.exit(0)
